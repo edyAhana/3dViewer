4dcce135a16936005335ae5fd03a2ee96d063201
Fedor Suhanov <fedorsuhanov150@gmail.com>
2026-03-08 11:10:57 +0300

    add shader src file

---
+++ engine/ShaderSource/fragment_shader.glsl
@@ -0,0 +1,9 @@
+#version 410 core
+
+in vec4 our_color;
+out vec4 color;
+
+void main()
+{
+    color = our_color;
+}
---
+++ engine/ShaderSource/vertex_shader.glsl
@@ -0,0 +1,13 @@
+#version 410 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 vertex_color;
+
+out vec4 our_color;
+
+void main()
+{
+    gl_Position = vec4(position.x, position.y, position.z, 1.0);
+    our_color = vec4(vertex_color, 1.0);
+
+}
